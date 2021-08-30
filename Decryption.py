950ee733b0037105a4224788856f2e40659d8b7d
doge404isfound <88475780+doge404isfound@users.noreply.github.com>
2021-08-30 20:21:55 +0545

    Add files via upload
    LOL!

---
+++ Decryption.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+file4 = input('enter key:')
+key = file4
+decryptor = Fernet(key)
+with open('mainfile.txt','rb')as z:
+    z_data = z.read()
+Decrypted_data = decryptor.decrypt(z_data)
+
+with open('mainfile.txt','wb') as z:
+    z.write(Decrypted_data)
+print('file is Decrypted')
+print(f"printing itsc content","mainfile.txt")
